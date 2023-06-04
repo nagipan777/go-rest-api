eaa4f6237bf38f879c5c7c5df2ab4de9e50cb52e
nagipan777 <nagipan777@gmail.com>
2023-06-04 23:20:43 +0900

    CORS 3

--- router/router.go
+++ router/router.go
@@ -13,7 +13,7 @@ import (
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{ /*"http://localhost:3000",*/ os.Getenv("FE_URL")},
+		AllowOrigins:     []string{"http://localhost:3000", os.Getenv("FE_URL")},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
